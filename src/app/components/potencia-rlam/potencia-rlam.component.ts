ff9381341f856e7c03e97759b09e1a59ad7c2e51
Bryan Torres <alvarezroger04@gmail.com>
2021-03-12 15:37:55 -0500

    Calulando la potencia

--- src/app/components/potencia-rlam/potencia-rlam.component.ts
+++ src/app/components/potencia-rlam/potencia-rlam.component.ts
@@ -9,12 +9,40 @@ export class PotenciaRlamComponent implements OnInit {
   @Input() base_numero: number = 0;
   @Input() exponente_numero: number = 0;
 
+  potencia: number = 0;
   constructor() {}
 
   ngOnInit(): void {}
 
   calcular() {
-    console.log(this.base_numero);
-    console.log(this.exponente_numero);
+    this.potencia = this.potencia_RALM(
+      Number(this.base_numero),
+      Number(this.exponente_numero)
+    );
+  }
+
+  multiplicar_RLAM(r: number, b: number) {
+    let suma: number = 0;
+    let i: number = 0;
+    while (i < r) {
+      suma += b;
+      i++;
+    }
+
+    return suma;
+  }
+
+  potencia_RALM(base: number, exponente: number) {
+    if (exponente === 0) {
+      return 1;
+    }
+
+    let acumulador = 1;
+    let i = 0;
+    while (i < exponente) {
+      acumulador = this.multiplicar_RLAM(acumulador, base);
+      i++;
+    }
+    return acumulador;
   }
 }
